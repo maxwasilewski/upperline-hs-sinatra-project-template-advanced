a5c0626213199bb72d00c03519da44d3b6cdbb4f
Max Wasilewski <maxwasil123@gmail.com>
2016-07-22 14:42:56 +0000

    Final Project videos work in this commit

--- application_controller.rb
+++ application_controller.rb
@@ -11,135 +11,156 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/brazil' do
-    @title = "Map2Music-#{}"
-    erb :brazil, :layout => :base
+    @title = "Map2Music-Brazil"
     @video_array = format_request_and_send_api_call("brazil")
+    @video_name = @video_array[1]
+    @channel_name = @video_array[2]
     @video_id = @video_array[0]
+    erb :brazil, :layout => :base
   end
   
   get '/southkorea' do
-    @title = "Map2Music-#{}"
-    erb :southkorea, :layout => :base
+    @title = "Map2Music-South Korea"
     @video_array = format_request_and_send_api_call("kpop")
     @video_id = @video_array[0]
+    erb :southkorea, :layout => :base
+
   end
   
   get '/jamaica' do
-    @title = "Map2Music-#{}"
-    erb :jamaica, :layout => :base
-    @video_array = format_request_and_send_api_call("jamaica")
+    @title = "Map2Music-Jamaica"
+    @video_array = format_request_and_send_api_call("steel drums")
     @video_id = @video_array[0]
+    erb :jamaica, :layout => :base
+    
   end
   
   get '/egypt' do
-    @title = "Map2Music-#{}"
-    erb :egypt, :layout => :base
+    @title = "Map2Music-Egypt"
     @video_array = format_request_and_send_api_call("egypt")
     @video_id = @video_array[0]
+    erb :egypt, :layout => :base
+    
   end
   
   get '/uae' do
-    @title = "Map2Music-#{}"
-    erb :uae, :layout => :base
+    @title = "Map2Music-UAE"
     @video_array = format_request_and_send_api_call("united arab emirates")
     @video_id = @video_array[0]
+    erb :uae, :layout => :base
+    
   end
   
   get '/saudiarabia' do
-    @title = "Map2Music-#{}"
-    erb :saudiarabia, :layout => :base
+    @title = "Map2Music-Saudi Arabia"
     @video_array = format_request_and_send_api_call("saudi arabia")
     @video_id = @video_array[0]
+    erb :saudiarabia, :layout => :base
+    
   end
   
   get '/russia' do
-    @title = "Map2Music-#{}"
-    erb :russia, :layout => :base
+    @title = "Map2Music-Russia"
     @video_array = format_request_and_send_api_call("russia")
     @video_id = @video_array[0]
+    erb :russia, :layout => :base
+    
   end
   
   get '/morocco' do
-    @title = "Map2Music-#{}"
-    erb :morocco, :layout => :base
+    @title = "Map2Music-Morocco"
     @video_array = format_request_and_send_api_call("morocco")
     @video_id = @video_array[0]
+    erb :morocco, :layout => :base
+    
   end
   
   get '/mexico' do
-    @title = "Map2Music-#{}"
-    erb :mexico, :layout => :base
+    @title = "Map2Music-Mexico"
     @video_array = format_request_and_send_api_call("mexico")
     @video_id = @video_array[0]
+    erb :mexico, :layout => :base
+    
   end
   
   get '/britain' do
-    @title = "Map2Music-#{}"
-    erb :britain, :layout => :base
-    @video_array = format_request_and_send_api_call("britain")
+    @title = "Map2Music-Britain"
+     @video_array = format_request_and_send_api_call("britain")
     @video_id = @video_array[0]
+    erb :britain, :layout => :base
+   
   end
   
   get '/germany' do
-    @title = "Map2Music-#{}"
-    erb :germany, :layout => :base
+    @title = "Map2Music-Germany"
     @video_array = format_request_and_send_api_call("germany")
     @video_id = @video_array[0]
+    erb :germany, :layout => :base
+    
   end
   
   get '/turkey' do
-    @title = "Map2Music-#{}"
-    erb :turkey, :layout => :base
+    @title = "Map2Music-Turkey"
     @video_array = format_request_and_send_api_call("turkey")
     @video_id = @video_array[0]
+    erb :turkey, :layout => :base
+    
   end
   get '/china' do
-    @title = "Map2Music-#{}"
-    erb :china, :layout => :base
+    @title = "Map2Music-China"
     @video_array = format_request_and_send_api_call("china")
     @video_id = @video_array[0]
+    erb :china, :layout => :base
+    
   end
   get '/spain' do
-    @title = "Map2Music-#{}"
-    erb :spain, :layout => :base
+    @title = "Map2Music-Spain"
     @video_array = format_request_and_send_api_call("spain")
     @video_id = @video_array[0]
+    erb :spain, :layout => :base
+    
   end
   get '/france' do
-    @title = "Map2Music-#{}"
-    erb :france, :layout => :base
+    @title = "Map2Music-France"
     @video_array = format_request_and_send_api_call("france")
     @video_id = @video_array[0]
+    erb :france, :layout => :base
+    
   end
   get '/southafrica' do
-    @title = "Map2Music-#{}"
-    erb :southafrica, :layout => :base
-    @video_array = format_request_and_send_api_call("south africa")
+    @title = "Map2Music-South Africa"
+    @video_array = format_request_and_send_api_call("folk south africa")
     @video_id = @video_array[0]
+    erb :southafrica, :layout => :base
+    
   end
   get '/india' do
-    @title = "Map2Music-#{}"
-    erb :india, :layout => :base
+    @title = "Map2Music-India"
     @video_array = format_request_and_send_api_call("india")
     @video_id = @video_array[0]
+    erb :india, :layout => :base
+    
   end
   get '/venezuela' do
-    @title = "Map2Music-#{}"
-    erb :venezuela, :layout => :base
-    @video_array = format_request_and_send_api_call("venezuela")
+    @title = "Map2Music-Venezuela"
+    @video_array = format_request_and_send_api_call("alma llanera")
     @video_id = @video_array[0]
+    erb :venezuela, :layout => :base
+    
   end
   get '/italy' do
-    @title = "Map2Music-#{}"
-    erb :italy, :layout => :base
+    @title = "Map2Music-Italy"
     @video_array = format_request_and_send_api_call("italy")
     @video_id = @video_array[0]
+    erb :italy, :layout => :base
+    
   end
   get '/unitedstates' do
-    @title = "Map2Music-#{}"
-    erb :unitedstates, :layout => :base
+    @title = "Map2Music-United States"
     @video_array = format_request_and_send_api_call("american jazz")
     @video_id = @video_array[0]
+    erb :unitedstates, :layout => :base
+    
   end
 end
 
